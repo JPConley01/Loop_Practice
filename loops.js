c28eda70e5d8bc12a9353d0c3ca626518e28539f
John Conley <jpconley01@gmail.com>
2016-05-21 16:27:51 +0000

    completed loops practice #1

---
+++ loops.js
@@ -0,0 +1,51 @@
+//practice 1
+for (var i =0; i < 501; i+= 100) {
+	console.log(i);
+}
+for(var i = 0; i < 6; i++) {
+	console.log(i*100);
+}
+
+//practice 2
+
+for (var i = 1; i < 65; i = i * 2 ) {
+	console.log(i);
+}	
+
+
+//practice 3
+
+for (var i = 0; i < 3; i++) {
+    console.log(+1+i);
+    console.log(+1+i);
+    console.log(+1+i);
+}
+
+
+//practice 4
+
+for (var i = 0; i < 6 ; i++) {
+	console.log(i * 2);
+}
+
+
+//practice 5
+
+for (var i = 0; i < 5 ; i++) {
+	console.log((+1+i) * 3)
+}
+
+//practice 6
+
+for (var i = 9; i > -1; i--) {
+	console.log(i)
+}
+
+//practice 7 
+ 
+for (var i = 0; i < 3; i++) {
+	for (var j = 0; j < 4; j++) {
+		console.log(j)
+	}
+	
+}
